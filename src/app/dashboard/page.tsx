68b1481f28df44d668f7d0f3b2d0d6e7cb0f8e06
Jun <jun55tsuno@gmail.com>
2023-02-20 13:37:05 -0800

    updated dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,31 @@
 "use client";
-import React from "react";
-import useAuth from "@/hooks/useAuth";
+import { useEffect } from "react";
 import HouseCard from "@/components/HouseCard";
+import useAuth from "@/hooks/useAuth";
+import { db } from "@/config/firebase";
+import { collection, DocumentData, getDocs, query } from "firebase/firestore";
 import AddHomeIcon from "@mui/icons-material/AddHome";
 
 const DashBoardPage = () => {
 	const { user } = useAuth();
 
-	const userName = user?.email;
+	let houseArr: DocumentData[] = [];
+
+	const fetchHouse = async () => {
+		const q = query(collection(db, `rent-manager/${user?.uid}/house`));
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach((doc) => {
+			houseArr.push(doc.data());
+		});
+		return houseArr;
+	};
+	fetchHouse();
+	console.log(houseArr);
 
 	return (
 		<>
 			<h2>
-				Welcome Back, <span className="font-bold">{`${userName}`}</span>
+				Welcome Back, <span className="font-bold"></span>
 			</h2>
 			<section>
 				<h3>Your House</h3>
